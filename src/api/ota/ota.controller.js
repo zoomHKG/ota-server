483f01ea967a4fe7aa6591faf453972b05a3d0cb
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-16 15:04:33 +0200

    send error response on failed binary fetch

--- src/api/ota/ota.controller.js
+++ src/api/ota/ota.controller.js
@@ -9,6 +9,7 @@ exports.getApp = (req, res) => {
   if (proj) {
     let version = 'v0.0.0'
     // version is sent by the ESP in request header
+    // TODO: verify device type before update
     if (proj.device === 'esp8266') {
       version = req.headers['x-esp8266-version'] || 'v0.0.0'
     }
@@ -27,6 +28,13 @@ exports.getApp = (req, res) => {
         })
         .catch(err => {
           eh.handleError(err)
+          res.send({
+            success: false,
+            error: {
+              code: 404,
+              message: 'Cannot fetch app binary'
+            }
+          })
         })
     }
   } else {
