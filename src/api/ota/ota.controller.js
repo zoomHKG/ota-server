654a80cc951072a7bc2cb2984f11e778e2b69173
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-16 14:51:41 +0200

    version check on ota controller

--- src/api/ota/ota.controller.js
+++ src/api/ota/ota.controller.js
@@ -3,21 +3,37 @@ const repo = system.getRepo()
 const rp = require('request-promise')
 
 exports.getApp = (req, res) => {
-  let proj = repo.getProject(req.params.project)
-  let fileName = proj.url.split('/').pop()
-  console.log(proj.url)
-  rp
-    .get(proj.url)
-    .then(res => {
-      console.log(Object.keys(res))
-      return res
+  const proj = repo.getProject(req.params.project)
+  if (proj) {
+    let version = 'v0.0.0'
+    // version is sent by the ESP in request header
+    if (proj.device === 'esp8266') {
+      version = req.headers['x-esp8266-version'] || 'v0.0.0'
+    }
+    // only update if newer version available
+    if (proj.version > version) {
+      const fileName = proj.url.split('/').pop()
+      rp
+        .get(proj.url)
+        .then(res => {
+          return res
+        })
+        .then(dat => {
+          res.set('Content-Type', 'application/octet-stream')
+          res.set('Content-Disposition', 'attachment; filename=' + fileName)
+          res.send(Buffer.alloc(dat.length, dat, 'binary'))
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  } else {
+    res.send({
+      success: false,
+      error: {
+        code: 401,
+        message: 'Project not found'
+      }
     })
-    .then(dat => {
-      res.set('Content-Type', 'application/octet-stream')
-      res.set('Content-Disposition', 'attachment; filename=' + fileName)
-      res.send(Buffer.alloc(dat.length, dat, 'binary'))
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  }
 }
