30b87e25dd19ce947e410cc4e6442ec449d20976
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-16 15:05:41 +0200

    added example request header from device

--- src/api/ota/ota.controller.js
+++ src/api/ota/ota.controller.js
@@ -4,6 +4,16 @@ const eh = require('../../utils')
 
 const repo = system.getRepo()
 
+/**
+ * Headers :
+ * {
+ * "host":"192.168.2.1:4000","user-agent":"ESP8266-http-Update","connection":"close",
+ * "x-esp8266-sta-mac":"84:F3:EB:4C:3D:B7","x-esp8266-ap-mac":"86:F3:EB:4C:3D:B7",
+ * "x-esp8266-free-space":"2863104","x-esp8266-sketch-size":"278848",
+ * "x-esp8266-sketch-md5":"1092358d7aea922e2c2af52983874b35","x-esp8266-chip-size":"4194304",
+ * "x-esp8266-sdk-version":"2.2.1(cfd48f3)","x-esp8266-mode":"sketch","x-esp8266-version":"0.1.0"
+ * }
+ */
 exports.getApp = (req, res) => {
   const proj = repo.getProject(req.params.project)
   if (proj) {
