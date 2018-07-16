71249a860017826c655c84c4b75aa9cd43a83cbb
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-16 15:05:12 +0200

    cleanup

--- src/api/repo/repo.controller.js
+++ src/api/repo/repo.controller.js
@@ -9,27 +9,9 @@ exports.getApps = (req, res) => {
   })
 }
 
-/**
- * Headers :
- * {
- * "host":"192.168.2.1:4000","user-agent":"ESP8266-http-Update","connection":"close",
- * "x-esp8266-sta-mac":"84:F3:EB:4C:3D:B7","x-esp8266-ap-mac":"86:F3:EB:4C:3D:B7",
- * "x-esp8266-free-space":"2863104","x-esp8266-sketch-size":"278848",
- * "x-esp8266-sketch-md5":"1092358d7aea922e2c2af52983874b35","x-esp8266-chip-size":"4194304",
- * "x-esp8266-sdk-version":"2.2.1(cfd48f3)","x-esp8266-mode":"sketch","x-esp8266-version":"0.1.0"
- * }
- */
 exports.getApp = (req, res) => {
-  // console.log(`Headers : ${JSON.stringify(req.headers, 2, null)}`)
-  // console.log(`version : ${req.headers['x-esp8266-version']}`)
   const proj = repo.getProject(req.params.project)
   if (proj) {
-    if (proj.device === 'esp8266') {
-      const version = req.headers['x-esp8266-version'] || 'v0.0.0'
-      if (proj.version > version) {
-        // console.log('Update Necessary')
-      }
-    }
     res.send({
       success: true,
       data: proj
