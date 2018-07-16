af12bdc597a3fbb868f8139860eef462b3946507
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-16 15:21:27 +0200

    updated api test

--- test/api.js
+++ test/api.js
@@ -13,6 +13,7 @@ let app
 let api
 const status = '/api/status'
 const repo = '/api/repo'
+const ota = '/api/ota'
 
 describe('API Tests', () => {
   before(() => {
@@ -62,6 +63,17 @@ describe('API Tests', () => {
   })
 
   describe('API endpoints /repo', () => {
+    it('should return success true', (done) => {
+      chai
+        .request(api)
+        .get(`${repo}/update`)
+        .then(res => {
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('object')
+          expect(res.body.success).to.be.eq(true)
+          done()
+        })
+    })
     it('should return test project', (done) => {
       chai
         .request(api)
@@ -71,6 +83,19 @@ describe('API Tests', () => {
           // chai.assert.isObject(res.body, 'Body should be an Object')
           expect(res.body).to.be.an('object')
           expect(res.body.data.device).to.be.eq('esp8266')
+          done()
+        })
+    })
+  })
+
+  describe('API endpoints /ota', () => {
+    it('should return binary file', (done) => {
+      chai
+        .request(api)
+        .get(`${ota}/test`)
+        .then(res => {
+          expect(res).to.have.status(200)
+          expect(res.headers['content-type']).to.be.eq('application/octet-stream')
           done()
         })
     })
