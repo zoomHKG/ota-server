1fc686b9e029ccacbfbdad00eb9f897de44e2665
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-25 20:47:30 +0200

    fixed root route

--- system/server/routes/index.js
+++ system/server/routes/index.js
@@ -4,7 +4,7 @@ const repoRoute = require('../../../src/api/repo')
 const otaRoute = require('../../../src/api/ota')
 
 module.exports = app => {
-  app.use('/', rootRoute)
+  app.use('/api', rootRoute)
   app.use('/api/status', statusRoute)
   app.use('/api/repo', repoRoute)
   app.use('/api/ota', otaRoute)
